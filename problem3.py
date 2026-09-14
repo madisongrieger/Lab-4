ce84816129f733cba9646c403b0d488c8aaef0f9
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-14 16:51:03 -0400

    Fix docstring formatting and example usage

--- problem3.py
+++ problem3.py
@@ -3,9 +3,9 @@ def pythagorean_triples(a,b,c):
   number number number -> boolean
   takes three parameters a,b,c and returns True if a^2+b^2=c^2 and False otherwise
 
-  >>> pythagorean_triples(2,4,5):
+  >>> pythagorean_triples(2,4,5)
   False
-  >>> pythagorean_triples (2,0,2):
+  >>> pythagorean_triples (2,0,2)
   True
   """
   if (a**2)+(b**2)==(c**2):
