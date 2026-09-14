92d82d6aafc79b1bfd4987f937b43391b0862eb3
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-14 15:52:38 -0400

    Fix formatting in problem1.py docstring

--- problem1.py
+++ problem1.py
@@ -4,9 +4,9 @@ def opposite_sign(a,b):
   takes two numbers a and b and returns True if a is greater than b and False otherwise
 
   >>> opposite_sign(2,4)
-  False 
+  False
   >>> opposite_sign(4,2)
-  True 
+  True
   """
   if a>b : 
     return True
