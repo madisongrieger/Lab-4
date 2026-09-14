1d68f20c6438ee35fc4e0af1e1000021f2beee1a
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-14 16:43:10 -0400

    Add pythagorean_triples function implementation
    Implement pythagorean_triples function to check if a, b, c form a Pythagorean triple.

--- problem3.py
+++ problem3.py
@@ -1,2 +1,14 @@
 def pythagorean_triples(a,b,c):
-  ## your code here
+  """
+  number number number -> boolean
+  takes three parameters a,b,c and returns True if a^2+b^2=c^2 and False otherwise
+
+  >>> pythagorean_triples(2,4,5):
+  False
+  >>>pythagorean_triples (2,0,2):
+  True
+  """
+  if a^2+b^2=c^2:
+    return True
+  else:
+    return False
