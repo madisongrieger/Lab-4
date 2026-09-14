cd787596f27a55120a53683c181552963603cade
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-14 17:18:38 -0400

    Improve docstring for wears_jacket function
    Update function docstring to clarify parameters and return type.

--- problem5.py
+++ problem5.py
@@ -1,6 +1,8 @@
 def wears_jacket(temp, raining):
   """
-  number -> boolean
-  
+  number boolean -> boolean
+  takes in a number named temp and a boolean value named raining and returns True if she wears a jacket.
+
+  >>>wears_jacket(50, True)
   
   ### your solution here
