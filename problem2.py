f67ff5c9fcc742c60d9153da2156f120fe88c7ab
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-14 16:17:14 -0400

    Add functionality to check divisibility
    Implemented the logic to check if n is a multiple of m.

--- problem2.py
+++ problem2.py
@@ -1,12 +1,20 @@
 def divisible_by_m(n,m):
   """
-  ## add to this as well as the doctests below
-
+  number number-> boolean 
+  take as input two numbers n and m and will return True if n is a multiple of m and False otherwise
+ 
   >>> divisible_by_m(3,2)
   False
   >>> divisible_by_m(0,4)
   True
   >>> divisible_by_m(-6, 2)
   True
+  >>> divisible_by_m(5,2)
+  False
+  >>> divisible_by_m(9,3)
+  True
   """
-  ## your code here
+  if n is a multiple of m: 
+  return True
+  else: 
+  return False 
