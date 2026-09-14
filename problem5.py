98726c08c9edbe1f2331d9854fd5a47014210508
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-14 17:24:53 -0400

    Add logic to determine if a jacket is needed
    Implement logic for wearing a jacket based on temperature and rain.

--- problem5.py
+++ problem5.py
@@ -3,6 +3,14 @@ def wears_jacket(temp, raining):
   number boolean -> boolean
   takes in a number named temp and a boolean value named raining and returns True if she wears a jacket.
 
-  >>>wears_jacket(50, True)
-  
-  ### your solution here
+  >>> wears_jacket(50, False)
+  True
+  >>> wears_jacket(70, True)
+  True
+  >>> wears_jacket(65, False)
+  False
+  """
+if temp <60 or raining:
+return True
+  else:
+return False
