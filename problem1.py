fc0cdcb6e7b24fad98aff6004dad55432bddf7a8
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-14 15:39:29 -0400

    Add opposite_sign function implementation
    Implement opposite_sign function to compare two numbers.

--- problem1.py
+++ problem1.py
@@ -1,2 +1,14 @@
 def opposite_sign(a,b):
-  ## your turn
+  """
+  whole number whole number-> boolean
+  takes two numbers a and b and returns True if a is greater than b and False otherwise
+
+  >>> opposite_sign(2,4)
+  False 
+  >>> opposite_sign(4,2)
+  True 
+  """
+  if a>b : 
+    return True
+  else:
+    return False 
