715abebfd40a6ae638409235592462ee963c8abe
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-17 08:57:29 -0400

    Correct triangle validation logic in is_triangle
    Fix the logic in is_triangle function to correctly determine if three sides can form a triangle.

--- problem4.py
+++ problem4.py
@@ -4,9 +4,9 @@ def is_triangle(a,b,c):
   takes three numbers a,b,c as input and returns True if these numbers can be the side lengths of a triangle and False otherwise
 
   >>> is_triangle(3,4,5)
-      True
+  True
   >>> is_triangle(3,6,9)
-      False
+  False
   """
   if a+b>c:
     return True
