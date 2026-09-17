d6515173d3d21e4bd549d8e88babc0b866cc634e
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-16 23:18:49 -0400

    Modify test cases in is_triangle docstring
    Updated example test cases in docstring for is_triangle function.

--- problem4.py
+++ problem4.py
@@ -3,9 +3,9 @@ def is_triangle(a,b,c):
   number number number -> boolean
   takes three numbers a,b,c as input and returns True if these numbers can be the side lengths of a triangle and False otherwise
 
-  >>> is_triangle(3,6,9)
+  >>> is_triangle(3,4,5)
       True
-  >>> is_triangle(4,5,8)
+  >>> is_triangle(3,6,9)
       False
   """
   if a+b>c:
