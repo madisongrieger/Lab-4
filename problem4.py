429aa6139805a52a7a9f683eceac80778eacd235
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-16 23:05:06 -0400

    Update is_triangle function implementation
    Fix function logic and update docstring for clarity.

--- problem4.py
+++ problem4.py
@@ -1,5 +1,14 @@
 def is_triangle(a,b,c):
   """
   number number number -> boolean
-  
-  ## your turn
+  takes three numbers a,b,c as input and returns True if these numbers can be the side lengths of a triangle and False otherwise
+
+  >>> is_triangle(3,6,9)
+      return True
+  >>> is_triangle(4,5,8)
+      return False
+  """
+  if a+b==c:
+    return True
+  else (a+b)!==c:
+    return False
